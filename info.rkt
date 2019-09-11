9c4d6643c08e7bdcfe9283d68df8d4894ace1a38
Valtýr Örn Kjartansson <valtyr@gmail.com>
2019-09-11 17:37:50 +0000

    Bæta við info.rkt

---
+++ info.rkt
@@ -0,0 +1,6 @@
+#lang info
+(define collection "Forritunarmál")
+(define deps '("base"))
+(define build-deps '("rackunit-lib"))
+(define pkg-desc "Skiladæmi í Forritunarmálum")
+(define version "0.1")
