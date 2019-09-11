b8956c9c3a530f1aca2bd8f7bc877fce987bb578
Valtýr Örn Kjartansson <valtyr@gmail.com>
2019-09-11 17:45:28 +0000

    Update name in info.rkt

--- info.rkt
+++ info.rkt
@@ -1,5 +1,5 @@
 #lang info
-(define collection "Forritunarmál")
+(define collection "forritunarmal")
 (define deps '("base"))
 (define build-deps '("rackunit-lib"))
 (define pkg-desc "Skiladæmi í Forritunarmálum")
