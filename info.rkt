72a2f6fe1c367ebcab9ed002264378ca69a1c4f2
Valtýr Örn Kjartansson <valtyr@gmail.com>
2019-09-11 18:39:13 +0000

    Delete info.rkt

--- info.rkt
+++
@@ -1,6 +0,0 @@
-#lang info
-(define collection "forritunarmal")
-(define deps '("base"))
-(define build-deps '("rackunit-lib"))
-(define pkg-desc "Skiladæmi í Forritunarmálum")
-(define version "0.1")
